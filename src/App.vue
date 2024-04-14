cf383a970884195ec24c3efd7adb1308259d4a1d
TheMartonfi <carlmartonfi@hotmail.com>
2024-04-14 11:07:22 -0600

    Update formatting

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,11 @@ import CountButton from './components/CountButton.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <CountButton :initialCounter="0" />
     </div>
   </header>
-
   <main>
     <TheWelcome />
   </main>
